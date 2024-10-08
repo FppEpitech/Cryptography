090db3cd0c1e799e2269543abff1a99469786f18
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-10-08 16:57:15 +0200

    feat(Xor): cypher message with xor
    I implement the cypher method in xor

---
+++ Xor/Xor.py
@@ -0,0 +1,20 @@
+class Xor:
+    def __init__(self, message : str, key : str) -> None:
+        self.message = message
+        self.key = key
+
+    def cypherMessage(self):
+        cypher=""
+        hexaXor=""
+        for i in range (0, len(self.message)):
+            hexaKey = hex(int(self.key[i*2] + self.key[i*2+1], 16))
+            hexaLetter = hex(int(self.message[i].encode("utf-8").hex(), 16))
+            xor = int(hexaLetter, 16) ^ int(hexaKey, 16)
+            if xor == 0:
+                hexaXor = "00"
+            else:
+                hexaXor = hex(xor).removeprefix("0x")
+            if (len(hexaXor) == 1):
+                hexaXor = "0" + hexaXor
+            cypher+=hexaXor
+        cypher = "".join(map(str.__add__, cypher[-2::-2] ,cypher[-1::-2]))
