0876640b2d8491bae0a2d58dff6a09927e9e9cd6
PerAlban <alban.peralta@epitech.eu>
2024-10-14 13:49:25 +0200

    feat(systemClass): Create empty class to store future system
    I need to create those empty class to link them to the array of function in our main

---
+++ PGP_XOR.py
@@ -0,0 +1,6 @@
+
+from Mode import Mode
+
+class PgpXor:
+    def __init__(self, mode : Mode, message : str, key : str):
+        return
---
+++ XOR.py
@@ -0,0 +1,6 @@
+
+from Mode import Mode
+
+class Xor:
+    def __init__(self, mode : Mode, message : str, key : str):
+        return
