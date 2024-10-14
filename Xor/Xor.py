4e3cc1ede9abbda7ea769d914717f31a590c7b92
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-10-14 12:42:52 +0200

    refactor(Xor): Type return values and variables
    I type the return of functions and the variables to cleanify the code

--- Xor/Xor.py
+++ Xor/Xor.py
@@ -10,29 +10,29 @@ class Xor:
         return xor_decrypt(self.message, self.key)[::-1]
 
 
-def xor_encrypt(message, key):
-    message_bytes = message.encode('utf-8')
-    key_bytes = bytes.fromhex(key)
+def xor_encrypt(message : str, key : str) -> str:
+    message_bytes : bytes = message.encode('utf-8')
+    key_bytes : bytes = bytes.fromhex(key)
 
-    block_size = len(key_bytes)
+    block_size : int = len(key_bytes)
 
     if len(message_bytes) % block_size != 0:
-        padding_length = block_size - (len(message_bytes) % block_size)
+        padding_length : int = block_size - (len(message_bytes) % block_size)
         message_bytes += b'\x00' * padding_length
 
-    cipher_bytes = bytearray()
+    cipher_bytes : bytearray = bytearray()
     for i in range(len(message_bytes)):
         cipher_bytes.append(message_bytes[i] ^ key_bytes[i % block_size])
 
     return cipher_bytes.hex()
 
-def xor_decrypt(cipher_hex, key):
-    cipher_bytes = bytes.fromhex(cipher_hex)
-    key_bytes = bytes.fromhex(key)
+def xor_decrypt(cipher_hex : str, key : str) ->str:
+    cipher_bytes : bytes = bytes.fromhex(cipher_hex)
+    key_bytes : bytes = bytes.fromhex(key)
 
-    block_size = len(key_bytes)
+    block_size : int = len(key_bytes)
 
-    decrypted_bytes = bytearray()
+    decrypted_bytes : bytearray = bytearray()
     for i in range(len(cipher_bytes)):
         decrypted_bytes.append(cipher_bytes[i] ^ key_bytes[i % block_size])
 
