d1c8c27f55a26ca47c7b26e78299cef416f6ddc2
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-10-15 23:48:35 +0200

    feat(AES): Create AES decryption
    I created the AES decryption with -b

--- Aes/AES.py
+++ Aes/AES.py
@@ -1,40 +1,36 @@
 
 from Abstract.ACrypt import ACrypt
 from Aes.KeyExpansion import KeyExpension
-from Aes.SBox import sbox
+from Aes.SBox import *
 
 class Aes(ACrypt):
     def __init__(self, key: str) -> None:
-        super().__init__("".join(reversed([key[i:i+2] for i in range(0, len(key), 2)])))
-        self.keyRound : KeyExpension = KeyExpension("".join(reversed([key[i:i+2] for i in range(0, len(key), 2)])))
+        super().__init__(key)
+        self.keyRound : KeyExpension = KeyExpension(key)
 
     def _encrypt(self, message: str) -> str:
-        aes : bytes = self.firstRound(message)
-        aes = self.loopRounds(aes)
-        aes = self.lastRound(aes)
-        return "".join(reversed([aes.hex()[i:i+2] for i in range(0, len(aes.hex()), 2)]))
+        aes : bytes = self.addRoundKey(message.encode('utf-8'), bytes.fromhex(self.key))
+        for i in range(9):
+            aes = self.subBytes(aes)
+            aes = self.shiftRows(aes)
+            aes = self.mixColumns(aes, self.mixSingleColumn)
+            aes = self.addRoundKey(aes, self.keyRound.getKeyRound(i+1))
+        aes = self.subBytes(aes)
+        aes = self.shiftRows(aes)
+        aes = self.addRoundKey(aes, self.keyRound.getKeyRound(10))
+        return aes.hex()
 
     def _decrypt(self, message: str) -> str:
-        return message
-
-    def firstRound(self, message : str) -> bytes:
-        block : bytes = message.encode('utf-8')
-        aes : bytes = self.addRoundKey(block, bytes.fromhex(self.key))
-        return aes
-
-    def loopRounds(self, block : bytes) -> bytes:
-        for i in range(9):
-            block = self.subBytes(block)
-            block = self.shiftRows(block)
-            block = self.mixColumns(block)
-            block = self.addRoundKey(block, self.keyRound.getKeyRound(i+1))
-        return block
-
-    def lastRound(self, block : bytes) -> bytes:
-        block = self.subBytes(block)
-        block = self.shiftRows(block)
-        block = self.addRoundKey(block, self.keyRound.getKeyRound(10))
-        return block
+        aes : bytes = self.addRoundKey(bytes.fromhex(message), self.keyRound.getKeyRound(10))
+        for i in range(9, 0, -1):
+            aes = self.invShiftRows(aes)
+            aes = self.invSubBytes(aes)
+            aes = self.addRoundKey(aes, self.keyRound.getKeyRound(i))
+            aes = self.mixColumns(aes, self.invMixSingleColumn)
+        aes = self.invShiftRows(aes)
+        aes = self.invSubBytes(aes)
+        aes = self.addRoundKey(aes, self.keyRound.getKeyRound(0))
+        return aes.decode('utf-8')
 
     def addRoundKey(self, block1 : bytes, block2 : bytes) -> bytes:
         newBlock : bytearray = bytearray()
@@ -48,11 +44,24 @@ class Aes(ACrypt):
             newBlock.append(sbox[block[i]])
         return newBlock
 
+    def invSubBytes(self, block : bytes) -> bytes:
+        newBlock : bytearray = bytearray()
+        for i in range(len(block)):
+            newBlock.append(sboxInv[block[i]])
+        return newBlock
+
     def shiftRows(self, block: bytes) -> bytes:
         shift = list(block)
         shift[1], shift[5], shift[9], shift[13] = shift[5], shift[9], shift[13], shift[1]
         shift[2], shift[6], shift[10], shift[14] = shift[10], shift[14], shift[2], shift[6]
         shift[3], shift[7], shift[11], shift[15] = shift[15], shift[3], shift[7], shift[11]
+        return bytes(shift)
+
+    def invShiftRows(self, block: bytes) -> bytes:
+        shift = list(block)
+        shift[1], shift[5], shift[9], shift[13] = shift[13], shift[1], shift[5], shift[9]
+        shift[2], shift[6], shift[10], shift[14] = shift[10], shift[14], shift[2], shift[6]
+        shift[3], shift[7], shift[11], shift[15] = shift[7], shift[11], shift[15], shift[3]
         return bytes(shift)
 
     def galoisMult(self, byte: int, power: int) -> int:
@@ -75,10 +84,18 @@ class Aes(ACrypt):
         newMatrix3 = self.galoisMult(matrix0, 3) ^ matrix1 ^ matrix2 ^ self.galoisMult(matrix3, 2)
         return [newMatrix0, newMatrix1, newMatrix2, newMatrix3]
 
-    def mixColumns(self, block: bytes) -> bytes:
+    def invMixSingleColumn(self, column: list) -> list:
+        matrix0, matrix1, matrix2, matrix3 = column
+        newMatrix0 = self.galoisMult(matrix0, 14) ^ self.galoisMult(matrix1, 11) ^ self.galoisMult(matrix2, 13) ^ self.galoisMult(matrix3, 9)
+        newMatrix1 = self.galoisMult(matrix0, 9) ^ self.galoisMult(matrix1, 14) ^ self.galoisMult(matrix2, 11) ^ self.galoisMult(matrix3, 13)
+        newMatrix2 = self.galoisMult(matrix0, 13) ^ self.galoisMult(matrix1, 9) ^ self.galoisMult(matrix2, 14) ^ self.galoisMult(matrix3, 11)
+        newMatrix3 = self.galoisMult(matrix0, 11) ^ self.galoisMult(matrix1, 13) ^ self.galoisMult(matrix2, 9) ^ self.galoisMult(matrix3, 14)
+        return [newMatrix0, newMatrix1, newMatrix2, newMatrix3]
+
+    def mixColumns(self, block: bytes, mix) -> bytes:
         Array = bytearray(16)
         for i in range(4):
             column = [block[i * 4], block[i * 4 + 1], block[i * 4 + 2], block[i * 4+ 3]]
-            mixed_column = self.mixSingleColumn(column)
+            mixed_column = mix(column)
             Array[i * 4], Array[i * 4 + 1], Array[i * 4 + 2], Array[i * 4 + 3] = mixed_column
         return bytes(Array)
